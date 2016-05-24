5e433d032ce8809ef6c03731eda6d181960600a9
pixie-grasper <himajinn13sei@gmail.com>
2016-05-24 15:46:24 +0900

    uint8_t stream -> Unicode stream

--- src/main.cc
+++ src/main.cc
@@ -8,9 +8,136 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+char* gets(char* s);
 #include <vector>
+#include <map>
 
-void eval(const std::vector<uint8_t>&) {
+class File {
+ private:
+  std::vector<uint8_t> source;
+  std::size_t index;
+
+  using Unicode = uint32_t;
+  using TokenID = uint64_t;
+  std::map<std::vector<Unicode>, TokenID> forward_map;
+  std::map<TokenID, std::vector<Unicode>> backword_map;
+  enum class SpecialTokenID {
+    nil = 0,
+    t, f,
+    lparents, rparents,
+    quote, quasiquote, comma, comma_at,
+    double_quote,
+    Max
+  };
+
+ public:
+  explicit File(std::vector<uint8_t>&& source_)
+      : source(std::move(source_)),
+        index(0),
+        forward_map{},
+        backword_map{} {
+    return;
+  }
+
+ private:
+  // it decodes from utf-8 stream
+  Unicode get_next_unicode() {
+    if (index >= source.size()) {
+      return 0;
+    }
+    auto c0 = static_cast<Unicode>(source[index]); index++;
+    if (c0 < 0x80) {
+      return c0;
+    } else if (c0 < 0xc2) {
+      return 0;
+    } else if (c0 < 0xe0) {
+      if (index >= source.size()) {
+        return 0;
+      }
+      auto c1 = static_cast<Unicode>(source[index]); index++;
+      if ((c1 & 0xc0) != 0x80) {
+        return 0;
+      }
+      return ((c0 & 0x1f) << 6) |
+              (c1 & 0x3f);
+    } else if (c0 < 0xf0) {
+      if (index + 1 >= source.size()) {
+        return 0;
+      }
+      auto c1 = static_cast<Unicode>(source[index]); index++;
+      auto c2 = static_cast<Unicode>(source[index]); index++;
+      if (((c1 & 0xc0) != 0x80) ||
+          ((c2 & 0xc0) != 0x80)) {
+        return 0;
+      }
+      return ((c0 & 0x0f) << 12) |
+             ((c1 & 0x3f) <<  6) |
+              (c2 & 0x3f);
+    } else if (c0 < 0xf8) {
+      if (index + 2 >= source.size()) {
+        return 0;
+      }
+      auto c1 = static_cast<Unicode>(source[index]); index++;
+      auto c2 = static_cast<Unicode>(source[index]); index++;
+      auto c3 = static_cast<Unicode>(source[index]); index++;
+      if (((c1 & 0xc0) != 0x80) ||
+          ((c2 & 0xc0) != 0x80) ||
+          ((c3 & 0xc0) != 0x80)) {
+        return 0;
+      }
+      return ((c0 & 0x07) << 18) |
+             ((c1 & 0x3f) << 12) |
+             ((c2 & 0x3f) <<  6) |
+              (c3 & 0x3f);
+    } else if (c0 < 0xfc) {
+      if (index + 3 >= source.size()) {
+        return 0;
+      }
+      auto c1 = static_cast<Unicode>(source[index]); index++;
+      auto c2 = static_cast<Unicode>(source[index]); index++;
+      auto c3 = static_cast<Unicode>(source[index]); index++;
+      auto c4 = static_cast<Unicode>(source[index]); index++;
+      if (((c1 & 0xc0) != 0x80) ||
+          ((c2 & 0xc0) != 0x80) ||
+          ((c3 & 0xc0) != 0x80) ||
+          ((c4 & 0xc0) != 0x80)) {
+        return 0;
+      }
+      return ((c0 & 0x03) << 24) |
+             ((c1 & 0x3f) << 18) |
+             ((c2 & 0x3f) << 12) |
+             ((c3 & 0x3f) <<  6) |
+              (c4 & 0x3f);
+    } else if (c0 < 0xfe) {
+      if (index + 4 >= source.size()) {
+        return 0;
+      }
+      auto c1 = static_cast<Unicode>(source[index]); index++;
+      auto c2 = static_cast<Unicode>(source[index]); index++;
+      auto c3 = static_cast<Unicode>(source[index]); index++;
+      auto c4 = static_cast<Unicode>(source[index]); index++;
+      auto c5 = static_cast<Unicode>(source[index]); index++;
+      if (((c1 & 0xc0) != 0x80) ||
+          ((c2 & 0xc0) != 0x80) ||
+          ((c3 & 0xc0) != 0x80) ||
+          ((c4 & 0xc0) != 0x80) ||
+          ((c5 & 0xc0) != 0x80)) {
+        return 0;
+      }
+      return ((c0 & 0x01) << 30) |
+             ((c1 & 0x3f) << 24) |
+             ((c2 & 0x3f) << 18) |
+             ((c3 & 0x3f) << 12) |
+             ((c4 & 0x3f) <<  6) |
+              (c5 & 0x3f);
+    } else {
+      return 0;
+    }
+  }
+};
+
+void eval(std::vector<uint8_t>&& stream) {
+  File file(std::move(stream));
   return;
 }
 
@@ -54,7 +181,7 @@ int main(int argc, char** argv) {
   close(fd);
 
   // do something
-  eval(file);
+  eval(std::move(file));
 
   return 0;
 }
