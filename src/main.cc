9bb918735099e819ef942d1c372dcf6da83eebd4
pixie-grasper <himajinn13sei@gmail.com>
2016-05-24 16:38:59 +0900

    Unicode stream -> TokenID stream

--- src/main.cc
+++ src/main.cc
@@ -24,9 +24,9 @@ class File {
   enum class SpecialTokenID {
     nil = 0,
     t, f,
-    lparents, rparents,
+    lparent, rparent,
     quote, quasiquote, comma, comma_at,
-    double_quote,
+    dot, dots,
     Max
   };
 
@@ -132,6 +132,154 @@ class File {
               (c5 & 0x3f);
     } else {
       return 0;
+    }
+  }
+
+  TokenID get_next_token() {
+    auto c0 = get_next_unicode();
+    while (c0 == ' ' || c0 == '\t' || c0 == '\r' || c0 == '\n') {
+      c0 = get_next_unicode();
+    }
+    switch (c0) {
+      case 0:
+        return static_cast<TokenID>(SpecialTokenID::nil);
+      case '(':
+        return static_cast<TokenID>(SpecialTokenID::lparent);
+      case ')':
+        return static_cast<TokenID>(SpecialTokenID::rparent);
+      case '\'':
+        return static_cast<TokenID>(SpecialTokenID::quote);
+      case '`':
+        return static_cast<TokenID>(SpecialTokenID::quasiquote);
+      case ',': {
+        auto old_index = index;
+        auto c1 = get_next_unicode();
+        if (c1 == '@') {
+          return static_cast<TokenID>(SpecialTokenID::comma_at);
+        } else {
+          index = old_index;
+          return static_cast<TokenID>(SpecialTokenID::comma);
+        }
+      }
+      case '"': {
+        std::vector<Unicode> token{};
+        for (;;) {
+          auto ck = get_next_unicode();
+          if (ck == '"') {
+            return regist(std::move(token));
+          } else if (ck == 0) {
+            return static_cast<TokenID>(SpecialTokenID::nil);
+          } else if (ck == '\\') {
+            ck = get_next_unicode();
+            switch (ck) {
+              case 't':
+                ck = '\t';
+                break;
+              case 'n':
+                ck = '\n';
+                break;
+              default:
+                break;
+            }
+          }
+          token.push_back(ck);
+        }
+      }
+      case ';':
+        while (!(c0 == '\r' || c0 == '\n' || c0 == 0)) {
+          c0 = get_next_unicode();
+        }
+        if (c0 == 0) {
+          return static_cast<TokenID>(SpecialTokenID::nil);
+        }
+        return get_next_token();
+      case '.':
+      case '+':
+      case '-': {
+        auto old_index = index;
+        auto c1 = get_next_unicode();
+        if ('0' <= c1 && c1 <= '9') {
+          index = old_index;
+          break;
+        } else if (c0 == '.') {
+          if (c1 == '.') {
+            if (get_next_unicode() == '.') {
+              return static_cast<TokenID>(SpecialTokenID::dots);
+            } else {
+              return 0;
+            }
+          } else {
+            index = old_index;
+            return static_cast<TokenID>(SpecialTokenID::dot);
+          }
+        } else {
+          index = old_index;
+          std::vector<Unicode> token{c0};
+          return regist(std::move(token));
+        }
+      }
+      case '#': {
+        auto c1 = get_next_unicode();
+        if (c1 == 't') {
+          return static_cast<TokenID>(SpecialTokenID::t);
+        } else if (c1 == 'f') {
+          return static_cast<TokenID>(SpecialTokenID::f);
+        } else if (c1 == '\\') {
+          auto c2 = get_next_unicode();
+          std::vector<Unicode> token{c0, c1, c2};
+          return regist(std::move(token));
+        } else {
+          return static_cast<TokenID>(SpecialTokenID::nil);
+        }
+      }
+      default:
+        break;
+    }
+    std::vector<Unicode> token{c0};
+    if (c0 == '.' || c0 == '+' || c0 == '-' || ('0' <= c0 && c0 <= '9')) {
+      bool dotted = c0 == '.';
+      for (;;) {
+        auto old_index = index;
+        auto ck = get_next_unicode();
+        if ((dotted && ck == '.') || !('0' <= ck && ck <= '9')) {
+          index = old_index;
+          break;
+        } else if (ck == '.') {
+          dotted = true;
+        }
+        token.push_back(ck);
+      }
+    } else {
+      for (;;) {
+        auto old_index = index;
+        auto ck = get_next_unicode();
+        if (ck == '!' || ck == '$' || ck == '%' || ck == '&' || ck == '*' ||
+            ck == '+' || ck == '-' || ck == '.' || ck == '/' || ck == ':' ||
+            ck == '<' || ck == '=' || ck == '>' || ck == '?' || ck == '@' ||
+            ck == '^' || ck == '_' || ck == '~' ||
+            ('a' <= ck && ck <= 'z') ||
+            ('A' <= ck && ck <= 'Z') ||
+            ('0' <= ck && ck <= '9')) {
+          token.push_back(ck);
+        } else {
+          index = old_index;
+          break;
+        }
+      }
+    }
+    return regist(std::move(token));
+  }
+
+  TokenID regist(std::vector<Unicode>&& token) {
+    auto it = forward_map.find(token);
+    if (it == forward_map.end()) {
+      auto new_id = static_cast<TokenID>(SpecialTokenID::Max) +
+                    static_cast<TokenID>(forward_map.size());
+      forward_map[token] = new_id;
+      backword_map[new_id] = std::move(token);
+      return new_id;
+    } else {
+      return it->second;
     }
   }
 };
